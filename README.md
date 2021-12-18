cabdf62f4ab7d4ca41698ef48d329608f2300432
SmartPolarBear <CleverCoolBear@outlook.com>
2021-12-18 17:26:01 +0800

    feat(kmp): common kmp implementations
    chore(readme): update README.md

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 KMP Algorithm
 KMP is a famous algorithm for string pattern matching
 
-This repository contains the implementation of KMP with DFA and NFA (or partial matching table) .
+This repository contains the implementation of KMP with DFA and NFA (or partial matching table) in **kmp.cpp**.
 
-This repository also contains a implementation that will match strings that has given number of errors.
+This repository also contains an implementation in **kmp_with_errors.cpp** that will match strings that has given number of errors.
+
+/doc directory contains the report for the data structure course on this algorithm.
 
 Reference
 The True Beauty of KMP: from DFA to NFA
--- kmp.cpp
+++ kmp.cpp
@@ -1,4 +1,129 @@
-//
-// Created by cleve on 12/18/2021.
-//
+#include <iostream>
 
+using namespace std;
+static inline constexpr size_t PATTERN_LEN = 128;
+
+namespace DFA
+{
+int dfa[CHAR_MAX][PATTERN_LEN]{};
+
+void construct(const std::string& pattern)
+{
+	dfa[pattern.at(0)][0] = 1;
+	for (int i = 1, x = 0; i < pattern.length(); i++)
+	{
+		for (char c = 0; c < CHAR_MAX; c++)
+		{
+			dfa[c][i] = dfa[c][x];
+		}
+
+		dfa[pattern.at(i)][i] = i + 1;
+		x = dfa[pattern.at(i)][x];
+	}
+}
+
+int match(const std::string& pattern, const std::string& text, int pos)
+{
+	int i = pos, j = 0;
+	for (; i < text.length() && j < pattern.length(); i++)
+	{
+		j = dfa[text.at(i)][j];
+	}
+
+	if (j == pattern.length())
+	{
+		return i - pattern.length();
+	}
+
+	return text.length();
+}
+
+void test()
+{
+	std::string m = "ABABCAAABABABCA";// 2 11
+
+	construct("ABC");
+
+	int pos = match("ABC", m, 0);
+	while (pos < m.length())
+	{
+		std::cout << pos << std::endl;
+		pos = match("ABC", m, pos + 1);
+	}
+}
+
+}
+
+namespace NFA
+{
+int nfa[PATTERN_LEN]{};
+
+void construct(const std::string& pattern)
+{
+	int j = 0;
+	for (int i = 1; i < pattern.length(); i++)
+	{
+		nfa[i] = j;
+
+		while (j > 0 && pattern[i] != pattern[j])
+		{
+			j = nfa[j];
+		}
+
+		if (pattern[i] == pattern[j])
+		{
+			j++;
+		}
+	}
+}
+
+int match(const std::string& pattern, const std::string& text, int pos)
+{
+	int i = pos, j = 0;
+	for (; i < text.length() && j < pattern.length(); i++)
+	{
+		while (j > 0 && text[i] != pattern[j])
+		{
+			j = nfa[j];
+		}
+
+		if (text[i] == pattern[j])
+		{
+			j++;
+		}
+	}
+
+	if (j == pattern.length())
+	{
+		return i - pattern.length();
+	}
+
+	return text.length();
+}
+
+void test()
+{
+	memset(nfa, 0, sizeof(nfa));
+
+	construct("AAAAB");
+	for (int i = 0; i < 5; i++)
+	{
+		cout << nfa[i] << " ";
+	}
+	cout << endl;
+
+	memset(nfa, 0, sizeof(nfa));
+
+
+	std::string m = "ABABCAAABABABCA";// 2 11
+
+	construct("ABC");
+
+	int pos = match("ABC", m, 0);
+	while (pos < m.length())
+	{
+		std::cout << pos << std::endl;
+		pos = match("ABC", m, pos + 1);
+	}
+}
+}
